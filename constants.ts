c58f5fb4606db1da9fc54fd4308400ffa0c78d7c
AndreiEgorov <andreyegorov1992@gmail.com>
2024-08-22 00:45:35 -0700

    add basic stack

---
+++ constants.ts
@@ -0,0 +1,6 @@
+export const AWS_ADMIN_GENERAL_ACCOUNT_ID = 654654460200;
+
+export const AWS_ADMIN_GENERAL = {
+  account: AWS_ADMIN_GENERAL_ACCOUNT_ID,
+  region: "us-west-2",
+};
--- lib/passwordless-auth-stack.ts
+++ lib/passwordless-auth-stack.ts
@@ -1,16 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { UserPool } from "aws-cdk-lib/aws-cognito";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class PasswordlessAuthStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export interface IPasswordlessAuthStack extends StackProps {}
+
+export class PasswordlessAuthStack extends Stack {
+  constructor(scope: Construct, id: string, props?: IPasswordlessAuthStack) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PasswordlessAuthQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const userPool = new UserPool(this, "UserPool");
   }
 }
+
+//TODO continue here: following this: https://catalog.workshops.aws/cognito-webauthn-passwordless/en-US/20-backend/21-cdk-init.
+// I  need to propagate env to my Stack so it could synth correctly
