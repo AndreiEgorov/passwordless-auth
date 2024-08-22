720f20d9763170b60645dd248c39ada7135d5f7c
AndreiEgorov <andreyegorov1992@gmail.com>
2024-08-22 00:20:09 -0700

    initial

---
+++ test/passwordless-auth.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PasswordlessAuth from '../lib/passwordless-auth-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/passwordless-auth-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PasswordlessAuth.PasswordlessAuthStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
